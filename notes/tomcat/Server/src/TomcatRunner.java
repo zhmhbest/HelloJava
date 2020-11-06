536c8531bd561f8fbde2d943bfb02cb65cf9bece
zhmhbest <zhmhbest@gmail.com>
2020-11-06 15:52:47 +0800

    Tomcat Server

--- notes/tomcat/Server/src/TomcatRunner.java
+++ notes/tomcat/Server/src/TomcatRunner.java
@@ -2,9 +2,7 @@ import org.apache.catalina.startup.Bootstrap;
 
 public class TomcatRunner {
     public static void main(String[] args) {
-        TomcatResources.defaultProperties();
         TomcatResources.loadProperties("conf/runner");
-        TomcatResources.defaultConfigurations();
         Bootstrap.main(args);
     }
 }
