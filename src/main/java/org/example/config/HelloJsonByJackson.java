2c9766333d84301ba1edbf91e08a4fa70588826b
zhmhbest <zhmhbest@gmail.com>
2021-05-06 16:37:29 +0800

    excel

--- src/main/java/org/example/config/HelloJsonByJackson.java
+++ src/main/java/org/example/config/HelloJsonByJackson.java
@@ -3,43 +3,17 @@ package org.example.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.config.entities.Person;
 
 
 public class HelloJsonByJackson {
-    static class Person {
-        private String name;
-        private int age;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-    }
 
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Person person = new Person();
-        person.setName("Tom");
-        person.setAge(40);
+        person.setId(0);
+        person.setName("Jackson");
+        person.setGender(1);
 
         String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(person);
         System.out.println(jsonString);
---
+++ src/main/java/org/example/config/entities/Person.java
@@ -0,0 +1,44 @@
+package org.example.config.entities;
+
+import lombok.Data;
+
+
+@Data
+public class Person {
+    private int id;
+    private String name;
+    private int gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
