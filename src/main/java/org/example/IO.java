a20a559f469d3724b1d8b1dfbcfacf6af47176d4
zhmhbest <zhmhbest@gmail.com>
2021-05-06 11:04:32 +0800

    properties

--- src/main/java/org/example/IO.java
+++ src/main/java/org/example/IO.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 
 public class IO {
+    public static InputStream getResourceStream(String name) {
+        return IO.class.getResourceAsStream(name);
+    }
+
     /**
      * 读取资源目录文本文件
      *
@@ -13,13 +18,13 @@ public class IO {
      * @return 文本内容
      */
     public static String readResourceText(String name) {
-        InputStream is = IO.class.getResourceAsStream(name);
+        InputStream is = getResourceStream(name);
         if (null != is) {
             try {
                 int fileSize = is.available();
                 byte[] buffer = new byte[fileSize];
                 int readSize = is.read(buffer);
-                if (readSize != fileSize) return null;
+                if (readSize != fileSize) { return null; }
                 return new String(buffer, 0, fileSize, StandardCharsets.UTF_8);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -28,7 +33,28 @@ public class IO {
         return null;
     }
 
+    public static String readText(String fileName) {
+        try {
+            return readText(new FileInputStream(fileName));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static String readText(InputStream is) {
+        try {
+            byte[] buff = new byte[is.available()];
+            int size = is.read(buff);
+            return new String(buff, 0, size, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println(readResourceText("/demo.json"));
+        System.out.println(readText("src/main/resources/demo.json"));
     }
 }
