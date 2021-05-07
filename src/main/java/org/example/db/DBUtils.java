bbe4eab1ae0ac8164e5bba0c97700d6be51c7afd
zhmhbest <zhmhbest@gmail.com>
2021-05-07 15:44:45 +0800

    DBUtils

---
+++ src/main/java/org/example/db/DBUtils.java
@@ -0,0 +1,141 @@
+package org.example.db;
+
+import java.sql.*;
+
+
+public class DBUtils {
+    static {
+        try {
+            // 注册JDBC
+            Class.forName("org.mariadb.jdbc.Driver");
+            // Class.forName("com.mysql.jdbc.Driver");
+            // Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static Connection getConnection(
+            String host, int port, String dbname,
+            String user, String password
+    ) {
+        Connection conn = null;
+        final String url = String.format("jdbc:mysql://%s:%d/%s", host, port, dbname);
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    interface SqlQueryCallBack {
+        void run(ResultSet rs) throws SQLException;
+    }
+
+    interface SqlUpdateCallBack {
+        void run(PreparedStatement ps) throws SQLException;
+    }
+
+    protected Connection conn;
+
+    public DBUtils(Connection conn) {
+        this.conn = conn;
+    }
+
+    public DBUtils(String host, int port, String dbname, String user, String password) {
+        this.conn = getConnection(host, port, dbname, user, password);
+    }
+
+    public void disconnect() {
+        try {
+            this.conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public boolean connected() {
+        return null != this.conn;
+    }
+
+    /**
+     * 查询数据
+     *
+     * @param sql      SQL语句
+     * @param callback 回调方法
+     */
+    public void query(String sql, SqlQueryCallBack callback) {
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = this.conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            callback.run(rs);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (null != rs) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+            if (null != stmt) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * 执行SQL，不考虑响应结果及影响范围
+     *
+     * @param sql SQL语句
+     */
+    public void execute(String sql) {
+        Statement stmt = null;
+        try {
+            stmt = this.conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (null != stmt) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * 插入/更新 数据
+     *
+     * @param sql      SQL语句
+     * @param callback 回调方法
+     */
+    public void update(String sql, SqlUpdateCallBack callback) {
+        PreparedStatement ps = null;
+        try {
+            ps = this.conn.prepareStatement(sql);
+            callback.run(ps);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (null != ps) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
--- src/main/java/org/example/db/HelloJDBC.java
+++ src/main/java/org/example/db/HelloJDBC.java
@@ -1,121 +1,55 @@
 package org.example.db;
 
-import java.sql.*;
-
-interface SqlBatchAddCallBack {
-    void run(PreparedStatement ps) throws SQLException;
-}
 
 public class HelloJDBC {
-    static {
-        try {
-            // 注册JDBC
-            Class.forName("org.mariadb.jdbc.Driver");
-            // Class.forName("com.mysql.jdbc.Driver");
-            // Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
-    }
+    public static void main(String[] args) {
+        DBUtils db = new DBUtils("localhost", 3306, "", "root", "root");
+        if (db.connected()) {
+            System.out.println("已连接");
+            // 创建库表
+            db.execute("CREATE DATABASE IF NOT EXISTS `test_db`");
+            System.out.println("建库");
+            db.execute("CREATE TABLE IF NOT EXISTS `test_db`.`test_tbl` ( id BIGINT, name VARCHAR(32), PRIMARY KEY(id) );");
+            System.out.println("建表");
 
-    public static Connection getConnection(
-            String host, int port, String dbname,
-            String user, String password
-    ) {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(
-                    String.format("jdbc:mysql://%s:%d/%s", host, port, dbname),
-                    user, password
-            );
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        return conn;
-    }
-
-    public static Connection getConnection(String dbname, String user, String password) {
-        return getConnection("localhost", 3306, dbname, user, password);
-    }
-
-    public static ResultSet executeQuery(Connection conn, String sql) {
-        ResultSet rs = null;
-        try {
-            Statement stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        return rs;
-    }
-
-
-    public static int executeUpdate(Connection conn, String sql, SqlBatchAddCallBack callback) {
-        PreparedStatement ps = null;
-        int size = 0;
-        try {
-            ps = conn.prepareStatement(sql);
-            callback.run(ps);
-            size = ps.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        } finally {
-            if (null != ps) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }
-        return size;
-    }
-
-    public static void main(String[] args) throws SQLException {
-        // 查询
-        Connection conn1 = getConnection("mysql", "root", "");
-        if (null != conn1) {
-            ResultSet rs = executeQuery(conn1, "SELECT Host,User,Password FROM user WHERE User='root';");
-            ResultSetMetaData meta = rs.getMetaData();
-            for (int columnIndex = 1; columnIndex <= meta.getColumnCount(); columnIndex++) {
-                System.out.printf(
-                        "Name:%s, TypeName:%s\n",
-                        meta.getColumnName(columnIndex),
-                        meta.getColumnTypeName(columnIndex)
-                );
-            }
-            while (rs.next()) {
-                System.out.printf("%s,%s,%s\n",
-                        rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3)
-                );
-            }
-            rs.close();
-            conn1.close();
-        }
-
-        // 修改
-        Connection conn2 = getConnection("nutzbook", "root", "");
-        if (null != conn2) {
-            int size = executeUpdate(conn2, "INSERT INTO tbl_user VALUES(?,?,?);", ps -> {
+            // 插入
+            db.update("INSERT INTO `test_db`.`test_tbl` VALUES(?,?);", ps -> {
                 ps.clearBatch();
                 // 1
-                ps.setInt(1, 21);
+                ps.setInt(1, 1);
                 ps.setString(2, "qwe");
-                ps.setString(3, "123");
                 ps.addBatch();
-                ps.executeBatch();
-                ps.clearBatch();
                 // 2
-                ps.setInt(1, 22);
+                ps.setInt(1, 2);
                 ps.setString(2, "asd");
-                ps.setString(3, "456");
                 ps.addBatch();
-                // ps.executeBatch();
+                // 3
+                ps.setInt(1, 3);
+                ps.setString(2, "zxc");
+                ps.addBatch();
+                //
+                ps.executeBatch();
             });
-            System.out.println(size);
-            conn2.close();
+            System.out.println("插入");
+
+            // 查询
+            db.query("SELECT * FROM `test_db`.`test_tbl`;", rs -> {
+                while (rs.next()) {
+                    System.out.printf("%d,%s\n",
+                            rs.getInt(1),
+                            rs.getString(2)
+                    );
+                }
+            });
+
+            // 删除库表
+            db.execute("DROP TABLE IF EXISTS `test_db`.`test_tbl`;");
+            System.out.println("删表");
+            db.execute("DROP DATABASE IF EXISTS `test_db`;");
+            System.out.println("删库");
+        } else {
+            System.out.println("连接失败");
         }
+        db.disconnect();
     }
 }
