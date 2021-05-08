eeea07737ebe17bee355e91b2c575699a58de351
zhmhbest <zhmhbest@gmail.com>
2021-05-08 15:59:29 +0800

    lombok

--- src/main/java/org/example/config/entities/Person.java
+++ src/main/java/org/example/config/entities/Person.java
@@ -9,30 +9,6 @@ public class Person {
     private String name;
     private int gender;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGender() {
-        return gender;
-    }
-
-    public void setGender(int gender) {
-        this.gender = gender;
-    }
-
     @Override
     public String toString() {
         return "Person{" +
