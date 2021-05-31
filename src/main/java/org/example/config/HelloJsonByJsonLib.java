fbd67fc556d7408828e06fdfb35047748a91c87d
zhmhbest <zhmhbest@gmail.com>
2021-05-31 11:16:25 +0800

    HelloJsonByJsonLib 补充

--- src/main/java/org/example/config/HelloJsonByJsonLib.java
+++ src/main/java/org/example/config/HelloJsonByJsonLib.java
@@ -4,25 +4,40 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.example.IO;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class HelloJsonByJsonLib {
     public static void main(String[] args) {
         final String text = IO.readResourceText("/demo.json");
         assert null != text;
 
-        JSONObject json = JSONObject.fromObject(text);
-        System.out.println(json.getString("key"));
-        System.out.println(json.getDouble("PI"));
-        JSONArray arr = json.getJSONArray("arr");
+        // Map转JSON对象
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("gender", false);
+        JSONObject mapJson = JSONObject.fromObject(map);
+        System.out.println(mapJson);
+
+        // 字符串转JSON对象
+        JSONObject textJson = JSONObject.fromObject(text);
+        System.out.println(textJson);
+        System.out.println(textJson.getString("key"));
+        System.out.println(textJson.getDouble("PI"));
+        JSONArray arr = textJson.getJSONArray("arr");
         System.out.println(arr.get(1));
-        JSONObject obj = json.getJSONObject("obj");
+        JSONObject obj = textJson.getJSONObject("obj");
         System.out.println(obj.getInt("x"));
         System.out.println(obj.getInt("y"));
 
+        // 因为JSONObject实现了Map接口，所以可以像操作Map一样操作JSONObject
+        // 编辑JSONObject
         obj.put("z", 789);
         System.out.println(obj);
 
-        String resultJson = json.toString();
-        System.out.println(resultJson);
+        // JSON对象转化为字符串
+        String resultText = textJson.toString();
+        System.out.println(resultText);
     }
 }
