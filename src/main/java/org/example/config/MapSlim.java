272652f5c0efdcb4db753308937a68dac8055a3d
zhmhbest <zhmhbest@gmail.com>
2021-06-03 13:51:46 +0800

    eliminateKeyByEmptyValue

--- src/main/java/org/example/config/MapSlim.java
+++ src/main/java/org/example/config/MapSlim.java
@@ -1,43 +1,44 @@
 package org.example.config;
 
 import net.sf.json.JSONObject;
+import java.util.*;
 import org.example.IO;
 
-import java.util.*;
-
 public class MapSlim {
-    public static void mapSlim(Map<String, Object> map) {
-        List<Object> keys =
-        new ArrayList<String>(map.keySet().toArray());
-        System.out.println(keys);
-        // for (Object key: keys) {
-        //     map.remove(key);
-            // Object val = map.get(key);
-            // if (val instanceof String) {
-            //     System.out.println(val.toString());
-            //     // 移除空串
-            //     if (val.toString().isEmpty()) {
-            //         map.remove(key);
-            //     }
-            // }
-            // else if (List.class.isAssignableFrom(val.getClass())) {
-            //     System.out.println("List");
-            //     // for(Object item: (List)val) {
-            //     //     System.out.println(item.toString());
-            //     // }
-            // }
-            // else if (Map.class.isAssignableFrom(val.getClass())) {
-            //     System.out.println("Map");
-            // }
-        // }
+    @SuppressWarnings("unchecked")
+    public static void eliminateKeyByEmptyValue(Map<String, Object> map) {
+        for (Object rawKey : map.keySet().toArray()) {
+            String key = rawKey.toString();
+            Object val = map.get(key);
+            if (val instanceof String) {
+                // 移除空串
+                if (val.toString().isEmpty()) {
+                    map.remove(key);
+                }
+            } else if (List.class.isAssignableFrom(val.getClass())) {
+                List<Object> subList = (List<Object>) val;
+                for (Object item : subList) {
+                    if (Map.class.isAssignableFrom(item.getClass())) {
+                        eliminateKeyByEmptyValue((Map<String, Object>) item);
+                    }
+                }
+            } else if (Map.class.isAssignableFrom(val.getClass())) {
+                eliminateKeyByEmptyValue((Map<String, Object>) val);
+            }
+        }
     }
 
+    // public static void sortMapByKey(Map<String, Object> map) {
+    //
+    // }
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         final String text = IO.readResourceText("/slim.json");
         assert null != text;
         JSONObject json = JSONObject.fromObject(text);
         System.out.println(json);
-        mapSlim(json);
+        eliminateKeyByEmptyValue(json);
         System.out.println(json);
     }
 }
