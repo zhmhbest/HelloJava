f6639ca058c8ff1766cdc3cd140fdf55e6d30e46
zhmhbest <zhmhbest@gmail.com>
2021-06-22 11:11:45 +0800

    en-de url

--- src/main/java/org/example/base/HelloHash.java
+++ src/main/java/org/example/base/HelloHash.java
@@ -6,13 +6,16 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HelloHash {
 
     public static String hashMd5(String message) {
-        MessageDigest messageDigest = null;
+        MessageDigest messageDigest;
         try {
             messageDigest = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
@@ -24,7 +27,7 @@ public class HelloHash {
     }
 
     public static String hashSha256(String message) {
-        MessageDigest messageDigest = null;
+        MessageDigest messageDigest;
         try {
             messageDigest = MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException e) {
@@ -42,7 +45,7 @@ public class HelloHash {
 
     public static String decodeBase64(String message) {
         BASE64Decoder decoder = new BASE64Decoder();
-        byte[] bytes = null;
+        byte[] bytes;
         try {
             bytes = decoder.decodeBuffer(message);
         } catch (IOException e) {
@@ -52,9 +55,31 @@ public class HelloHash {
         return new String(bytes);
     }
 
+    public static String encodeUrl(String message) {
+        try {
+            return URLEncoder.encode(message, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String decodeUrl(String message) {
+        try {
+            return URLDecoder.decode(message, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(hashMd5("123"));
         System.out.println(hashSha256("123"));
+
+        String url = encodeUrl("https://cn.bing.com/");
+        System.out.println(url);
+        System.out.println(decodeUrl(url));
 
         String base64 = encodeBase64("123");
         System.out.println(base64);
