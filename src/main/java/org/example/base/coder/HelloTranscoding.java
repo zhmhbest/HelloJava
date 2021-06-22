6c40898d92f72f6ac1d773b927f96b1e3a6d0e31
zhmhbest <zhmhbest@gmail.com>
2021-06-22 11:37:52 +0800

    coder

--- src/main/java/org/example/base/coder/HelloTranscoding.java
+++ src/main/java/org/example/base/coder/HelloTranscoding.java
@@ -1,7 +1,5 @@
-package org.example.base;
+package org.example.base.coder;
 
-
-import org.apache.commons.codec.binary.Hex;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
@@ -9,35 +7,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-public class HelloHash {
-
-    public static String hashMd5(String message) {
-        MessageDigest messageDigest;
-        try {
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
-        }
-        byte[] ciphertext = messageDigest.digest(message.getBytes());
-        return Hex.encodeHexString(ciphertext);
-    }
-
-    public static String hashSha256(String message) {
-        MessageDigest messageDigest;
-        try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
-        }
-        byte[] ciphertext = messageDigest.digest(message.getBytes());
-        return Hex.encodeHexString(ciphertext);
-    }
-
+public class HelloTranscoding {
     public static String encodeBase64(String message) {
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(message.getBytes());
@@ -74,9 +45,6 @@ public class HelloHash {
     }
 
     public static void main(String[] args) {
-        System.out.println(hashMd5("123"));
-        System.out.println(hashSha256("123"));
-
         String url = encodeUrl("https://cn.bing.com/");
         System.out.println(url);
         System.out.println(decodeUrl(url));
