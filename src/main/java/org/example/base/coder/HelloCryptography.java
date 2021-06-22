daf9c78bb9315d3998f41b73afdd908b0d13dc0e
zhmhbest <zhmhbest@gmail.com>
2021-06-22 11:38:24 +0800

    coder

--- src/main/java/org/example/base/coder/HelloCryptography.java
+++ src/main/java/org/example/base/coder/HelloCryptography.java
@@ -6,7 +6,7 @@ package org.example.base.coder;
         常见算法：DES、3DES、AES、RC
     【非对称加密】
         有公钥（public key）和私钥（private key）
- 常见算法：RSA、DSA、ECDSA、ED25519
+        常见算法：RSA、DSA、ECDSA、ED25519
         * 身份验证：私钥签名，公钥验证
         * 定向传输：公钥加密，私钥解密
  */
